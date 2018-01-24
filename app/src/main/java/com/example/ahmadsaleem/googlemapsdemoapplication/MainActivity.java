fc71f16ab943860a783a49d185cc452762e2ab92
ahmad <ahmdsaleem@hotmail.com>
2018-01-24 15:30:55 +0500

    Google Maps Api 2 demo 1

---
+++ app/src/main/java/com/example/ahmadsaleem/googlemapsdemoapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.ahmadsaleem.googlemapsdemoapplication;
+
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    GoogleMap mGoogleMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(googleServicesAvailable())
+        {
+            Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.activity_main);
+            initMap();
+        }
+        else
+        {
+            //services not found view
+        }
+    }
+
+    private void initMap() {
+        MapFragment mapFragment= (MapFragment) getFragmentManager().findFragmentById(R.id.activity_main_mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    public boolean googleServicesAvailable()
+    {
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int isAvailable = googleApiAvailability.isGooglePlayServicesAvailable(this);
+        if(isAvailable==ConnectionResult.SUCCESS)
+        {
+            return true;
+        }
+        else if(googleApiAvailability.isUserResolvableError(isAvailable))
+        {
+            Dialog dialog=googleApiAvailability.getErrorDialog(this,isAvailable,0);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this,"Cant Connect to Play Services",Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+    mGoogleMap= googleMap;
+    goToLocationZoom(39.008225, -76.8984527,15);
+    }
+
+    private void goToLocation(double lat, double lng) {
+        LatLng latLng= new LatLng(lat,lng);
+        CameraUpdate cameraUpdate= CameraUpdateFactory.newLatLng(latLng);
+        mGoogleMap.moveCamera(cameraUpdate);
+    }
+
+    private void goToLocationZoom(double lat, double lng,float zoom) {
+        LatLng latLng= new LatLng(lat,lng);
+        CameraUpdate cameraUpdate= CameraUpdateFactory.newLatLngZoom(latLng,zoom);
+        mGoogleMap.moveCamera(cameraUpdate);
+    }
+
+}
+
